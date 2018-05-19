6f6fcd5aa8616ad03d900ced5113d650033caaca
SpiritQAQ <444042073@qq.com>
2018-05-19 21:18:30 +0800

    router

--- src/components/Gouwuche.vue
+++ src/components/Gouwuche.vue
@@ -3,12 +3,22 @@
     <div class="g-header">
       
     </div>
+    <div class="g-content">
+      <router-link to="/test">Go to Test</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name:"Gouwuche",
+  data(){
 
+  },
+  mounted(){
+    
+  }
+  
 }
 </script>
 
